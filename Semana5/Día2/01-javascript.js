976e2bcf19a5e86e7fe40442a3c3e82e16f0a6a0
Jorge Montesinos <osmarmontes50@gmail.com>
2025-07-30 22:19:42 -0500

    avance

--- Semana5/Día2/01-javascript.js
+++ Semana5/Día2/01-javascript.js
@@ -15,8 +15,20 @@ console.log(typeof edad2);
 
 let estaLloviendo = true; //Boolean true/false
 console.log(estaLloviendo);
+console.log(typeof estaLloviendo);
 
 //js no tiene tipado estático, por lo que podemos cambiar el tipo de una variable
 edad2 = 25; // Cambiando el valor de edad2 a un número
 console.log(edad2);
 console.log(typeof edad2);
+
+//mediante la , separamos varios valores para imprimir
+console.log("Este es un texto independiente", "este es otro", 123, true, false);
+
+let noDefinido;
+console.log("valor:", noDefinido);
+console.log("tipo de dato", typeof noDefinido); //undefined
+
+let nulo = null; //para que eventualmente se asigne un valor
+console.log("valor:", nulo);
+console.log("tipo de dato", typeof nulo); //object
