a414a0ce51ae0dcb057abb0cda1a63f4a5254e3a
Jorge Montesinos <osmarmontes50@gmail.com>
2025-07-30 22:48:09 -0500

    avance

--- Semana5/Día2/01-javascript.js
+++ Semana5/Día2/01-javascript.js
@@ -44,5 +44,21 @@ console.log(100 + 5); //expresión, la parte del 100 + 5 se evalúa y devuelve u
 console.log("Suma:", 10 + 5); // Suma
 console.log("Resta:", 10 - 5); // Resta
 console.log("Multiplicación", 10 * 5); // Multiplicación
-console.log("División", 10 / 5); // División
+console.log("División", 10 / 3); // División
 console.log("Módulo", 10 % 3); // Módulo (residuo de la división)
+// console.log(parseInt(10 / 3));
+
+//concatenación, el + está siendo usado para unir strings
+console.log("Hola" + " " + "Mundo"); // Concatenación de cadenas
+//CUIDADO, si uno de los valores es un string, se convierte todo a string
+console.log("Vamos a hacer una suma: ", "10" + 5);
+
+// Template Strings, Template Literals
+let nombreCompleto = "Juan Pérez";
+console.log("Hola " + nombreCompleto + ", ¿cómo estás?"); // Concatenación tradicional no recomendada
+
+console.log(`
+  Hola ${nombreCompleto},
+  ¿cómo estás?, Aquí tengo los ${10 + 20}
+  que te debía.
+`); // Usando template literals
