a998f0b58ca45d6396aa4014fd7c581710064fe5
Jorge Montesinos <osmarmontes50@gmail.com>
2025-07-30 22:07:52 -0500

    avance

--- Semana5/Día2/01-javascript.js
+++ Semana5/Día2/01-javascript.js
@@ -12,3 +12,11 @@ console.log(typeof primerNombre);
 let edad2 = '30'; //String
 console.log(edad2);
 console.log(typeof edad2);
+
+let estaLloviendo = true; //Boolean true/false
+console.log(estaLloviendo);
+
+//js no tiene tipado estático, por lo que podemos cambiar el tipo de una variable
+edad2 = 25; // Cambiando el valor de edad2 a un número
+console.log(edad2);
+console.log(typeof edad2);
