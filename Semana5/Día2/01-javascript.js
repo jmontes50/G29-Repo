740262a9167b6ffa675e72ecd2106ee66f2f720b
Jorge Montesinos <osmarmontes50@gmail.com>
2025-07-30 21:58:40 -0500

    avance

---
+++ Semana5/Día2/01-javascript.js
@@ -0,0 +1,14 @@
+console.log("Hola, estudiantes!");
+console.log("Bienvenidos a JavaScript.");
+
+let edad = 30; // Número
+console.log(edad);
+console.log(typeof edad);
+
+let primerNombre = "Juan"; //String
+console.log(primerNombre);
+console.log(typeof primerNombre);
+
+let edad2 = '30'; //String
+console.log(edad2);
+console.log(typeof edad2);
