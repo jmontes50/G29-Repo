855c7988f84b3631cc9d6c1e7e1b88363f23232e
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-01 22:10:18 -0500

    avance

--- Semana5/Día3/04-ejercicio05.js
+++ Semana5/Día3/04-ejercicio05.js
@@ -13,10 +13,42 @@ let total = prompt("Cuál fue el monto consumido?");
 //si total es "diez" -> isNaN me dará: true
 // console.log(isNaN(total));
 //Si !isNaN("diez") me va a dar false
+
+/*
 if(!isNaN(total)){
   let totalNumber = Number(total);
   let montoPorPersona = totalNumber / 3;
-  alert(`El monto a pagar equitativo por c/u es: ${montoPorPersona}`)
+  alert(`El monto a pagar equitativo por c/u es: ${montoPorPersona}`);
 } else {
   console.log("Ingrese un valor válido");
 }
+*/
+
+/*
+//versión normal
+if(isNaN(total)){
+  console.log("Ingrese un valor válido");
+} else {
+  let totalNumber = Number(total);
+  let montoPorPersona = totalNumber / 3;
+  alert(`El monto a pagar equitativo por c/u es: ${montoPorPersona}`);
+}
+*/
+
+
+//versión evaluando si es positivo
+debugger;
+let esNumero = !isNaN(total);
+debugger;
+let esPositivo = total >= 0;
+debugger;
+if( esNumero && esPositivo){
+  debugger;
+  let totalNumber = Number(total);
+  debugger;
+  let montoPorPersona = totalNumber / 3;
+  alert(`El monto a pagar equitativo por c/u es: ${montoPorPersona}`);
+} else {
+  console.log("Ingrese un valor válido");
+}
+
