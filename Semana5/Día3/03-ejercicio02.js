ddc58cec7db6beef61b5eb2234457449fba43c35
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-01 21:01:43 -0500

    avance

--- Semana5/Día3/03-ejercicio02.js
+++ Semana5/Día3/03-ejercicio02.js
@@ -6,10 +6,10 @@ if(isNaN(numero)) {
   //aquí ya es un número, parseFloat convierte el string a un número decimal
   let numeroConvertido = parseFloat(numero);
   if (numeroConvertido === 0){
-    console.log("Es igual a 0");
+    console.log(`${numeroConvertido} es Cero`);
   } else if (numeroConvertido < 0){
-    console.log("Es negativo");
+    console.log(`${numeroConvertido} es negativo`);
   } else {
-    console.log("Es positivo");
+    console.log(`${numeroConvertido} es positivo`);
   }
 }
