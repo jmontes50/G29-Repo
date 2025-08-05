e68fca25a101052ffce725852ef13d8d009ba96a
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-04 22:53:30 -0500

    avance

--- Semana6/Día1/05-arrayfor.js
+++ Semana6/Día1/05-arrayfor.js
@@ -3,9 +3,29 @@ let frutas = ["Kiwi", "Naranja", "Mango", "Manzana"];
 console.log("Longitud:", frutas.length); //la longitud, cantidad de elementos en array, ej: 4
 console.log("Item individual por índice:", frutas[2]) //Mango
 
+/*
 //Con un for recorrer el arreglo
 for (let i = 0; i < frutas.length; i++){
   console.log("inicializador",i);
   console.log(frutas[i])
 }
+*/
 
+//string como arreglo
+let palabra = "hipopotamo";
+
+console.log("Letra:", palabra[4]);
+
+let cantidadLetraO = 0;
+
+for(let i = 0; i < palabra.length; i++){
+  // console.log(palabra[i]);
+  console.log(palabra[i])
+  if(palabra[i] === "o"){
+    //si encontramos una o, la contamos
+    console.log("EUREKA")
+    cantidadLetraO++
+  }
+}
+
+console.log(cantidadLetraO);
