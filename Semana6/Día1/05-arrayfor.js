4a19e30a416cc68ffbd9d54f2a61534168c6e7ea
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-04 22:44:41 -0500

    avance

---
+++ Semana6/Día1/05-arrayfor.js
@@ -0,0 +1,11 @@
+let frutas = ["Kiwi", "Naranja", "Mango", "Manzana"];
+
+console.log("Longitud:", frutas.length); //la longitud, cantidad de elementos en array, ej: 4
+console.log("Item individual por índice:", frutas[2]) //Mango
+
+//Con un for recorrer el arreglo
+for (let i = 0; i < frutas.length; i++){
+  console.log("inicializador",i);
+  console.log(frutas[i])
+}
+
