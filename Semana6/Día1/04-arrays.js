276b8f0b0b996fcfce4d7ac5b4f5b164f323b5f0
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-04 22:20:37 -0500

    avance

---
+++ Semana6/Día1/04-arrays.js
@@ -0,0 +1,30 @@
+// let alumno1 = "Alexys";
+// let alumno2 = "Antonio";
+// let alumno3 = "Braulio";
+//...
+//Array - Arreglo
+// let alumnos = ["Alexys", "Antonio", "Braulio", "Bryan", "Carlos", "Cesar", "Cielo"]
+let alumnos = [
+  "Alexys",
+  "Antonio",
+  "Braulio",
+  "Bryan",
+  "Carlos",
+  "Cesar",
+  "Cielo",
+  "Eduardo",
+  "Franco"
+];
+
+console.log(alumnos);
+//TODO Array va a tener es un índice, index o posición Y SIEMPRE desde 0
+
+console.log(alumnos[5]);
+console.log(alumnos[3]);
+console.log(alumnos[2]);
+
+//Longitud me indica la cantidad de items en el array, no cuenta desde 0, es la cantidad directamente
+console.log(alumnos.length);
+
+//
+console.log("viendo item al final:", alumnos[alumnos.length - 1]);
