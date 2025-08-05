4b11325cbf44e55c3b1d164a54ded971c87c5212
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-04 20:52:44 -0500

    avance

--- Semana6/Día1/02-while-do.js
+++ Semana6/Día1/02-while-do.js
@@ -4,10 +4,13 @@ while(expresión a evaluar){
   y mientras esa expresion seá válida seguirá ejecutando el bloque de codigo entre sus {}
 }
 */
+
+/*
 let cantidadApps = 1;
 // debugger;
 
 while(cantidadApps < 15){
+  //template literal
   console.log(`Puede instalar apps en su móvil ${cantidadApps}`);
   //actualizar el contador o la expresión para que en algún momento sea falsa y se detenga
   cantidadApps = cantidadApps + 1;
@@ -16,3 +19,22 @@ while(cantidadApps < 15){
 }
 //cuando ya termina de ejecutarse,recién ejecuta las siguiente lineas
 console.log("Ya no puede instalar Apps");
+*/
+
+//Do-while
+
+/*
+do {
+  código a ejecutar al menos una vez
+} while (expresión a evaluar para seguir ejecutando el bloque)
+*/
+
+let tienePermisos = false;
+
+do {
+  console.log(`Evaluando al incio tienePermisos: ${tienePermisos}`);
+  tienePermisos = confirm("El usuario tiene permisos?")
+  console.log(`Observando el valor de tienePermisos: ${tienePermisos}`);
+} while (tienePermisos === true);
+
+console.log("Fin de script", tienePermisos);
