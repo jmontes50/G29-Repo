7950e24da2bfbfd21888a30846e8f2f7e1c49bbe
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-04 21:40:33 -0500

    avance

--- Semana6/Día1/03-for.js
+++ Semana6/Día1/03-for.js
@@ -6,8 +6,24 @@
 
 //apps++ <---------> apps = apps + 1, es lo mismo
 // for(let apps = 0; apps <= 10; apps++){
+/*
 for(let apps = 0; apps <= 10; apps+= 2){
   console.log(`Instalando app: ${apps}`);
 }
 
 console.log("Ya no tiene espacio");
+*/
+
+let limite = Number(prompt("Hasta que número deseas sumar?"));
+
+let suma = 0;
+
+for(let i = 0; i <= limite; i++) {
+  suma = suma + i;
+  // debugger;
+  console.log(suma);
+  // debugger;
+  // suma+=i;
+}
+
+console.log(`La suma del 1 al ${limite} es ${suma}`);
