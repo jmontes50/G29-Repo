c61e4a449575c07bcfe4ab446be77aa74e9232f6
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-04 22:29:55 -0500

    avance

--- Semana6/Día1/04-arrays.js
+++ Semana6/Día1/04-arrays.js
@@ -13,8 +13,8 @@ let alumnos = [
   "Cesar",
   "Cielo",
   "Eduardo",
-  "Franco"
-];
+  "Franco",
+]; //si el código crece mucho a la der, alt + shift + f
 
 console.log(alumnos);
 //TODO Array va a tener es un índice, index o posición Y SIEMPRE desde 0
@@ -28,3 +28,20 @@ console.log(alumnos.length);
 
 //
 console.log("viendo item al final:", alumnos[alumnos.length - 1]);
+
+//No tiene que guardar necesariamente el mismo tipo de dato 😺😺😺
+let items = ["Gabriel", 100, true, undefined, ["foo", "bar"]];
+console.log("dentro de items", items[1]);
+
+// let matriz = [[4,5,6], [7,8,9], [10,11,12]]
+let matriz = [
+  [4, 5, 6],
+  [7, 8, 9],
+  [10, 11, 12],
+];
+
+console.log(matriz[1]);
+
+//matriz[numero] hace referencia a un items que es un array dentro de otro
+//matriz[numero][numero] hace referencia a un item dentro del array dentro del otro array
+console.log(matriz[1][2]);
