8b41865fb0a159a9d8997d8c2f6b94e214ac75cd
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-08 20:36:16 -0500

    avance

--- Semana6/Día3/02-funciones.js
+++ Semana6/Día3/02-funciones.js
@@ -9,13 +9,28 @@ saludar(); //ejecución/invocación de la función con ()
 console.log("referencia función: ", saludar);
 // console.log("ejecución/invocación de la función: ", saludar());
 
+let resta = 0;
+
 //parámetros, como una declaración de variables
-function suma(num1, num2){
+function sumatoria(num1, num2){
   // console.log(num1 + num2);
-  let resSuma = num1 + num2;
-  return resSuma; //retornando la expresión con el resultado
+  let result = num1 + num2;
+  resta = num2 - num1;
+  // console.log(result);
+  // return num1 + num2;
+  return result; //retornando la expresión con el resultado
 }
 
-let resultado = suma(10, 20); //argumentos, valores de los parámetros
+
+let resultado = sumatoria(10, 20); //argumentos, valores de los parámetros
 
 console.log("resultado", resultado);
+
+console.log("Resta: ", resta);
+
+//función pura
+//Es una función que no es afectada por algo externo
+function multiplicacion (a, b){
+  return a * b;
+}
+console.log("Multiplicación:", multiplicacion(4, 7));
