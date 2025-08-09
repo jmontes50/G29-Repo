58214f9db22cc167d6c2672b691574cec30ab5e5
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-08 20:07:57 -0500

    avance

--- Semana6/Día3/02-funciones.js
+++ Semana6/Día3/02-funciones.js
@@ -8,3 +8,14 @@ saludar(); //ejecución/invocación de la función con ()
 
 console.log("referencia función: ", saludar);
 // console.log("ejecución/invocación de la función: ", saludar());
+
+//parámetros, como una declaración de variables
+function suma(num1, num2){
+  // console.log(num1 + num2);
+  let resSuma = num1 + num2;
+  return resSuma; //retornando la expresión con el resultado
+}
+
+let resultado = suma(10, 20); //argumentos, valores de los parámetros
+
+console.log("resultado", resultado);
