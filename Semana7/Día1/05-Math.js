ca2d5ce1ecfb8d58c0d56e86221ed9027dec12fb
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-11 21:38:39 -0500

    avance

---
+++ Semana7/Día1/05-Math.js
@@ -0,0 +1,21 @@
+console.log("Num. PI", Math.PI);
+
+console.log("Num. Euler", Math.E);
+
+console.log("Raíz cuadrada", Math.sqrt(64));
+
+let nota = 18.5;
+
+console.log("Redondear", Math.round(nota));
+
+nota = 18.1;
+
+console.log("Ceil, redondear al núm cercano superior", Math.ceil(nota));
+
+nota = 18.9;
+
+console.log("floor, redonde al núm inferior", Math.floor(nota));
+
+let aleatorio = Math.random(); //num aleatorio entre 0 y 1
+
+console.log("Aleatorio", aleatorio);
