9c288cdc2159a5fbb53fde37c196bc74f260c742
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-11 20:42:35 -0500

    avance

--- Semana7/Día1/03-objetosestandar.js
+++ Semana7/Día1/03-objetosestandar.js
@@ -9,9 +9,22 @@ let gato = {
     console.log("ZZZZZZZZZZZZZZ")
   }
 }
-
+/*
 console.log(gato.nombre);
 console.log(gato["edad"])
 //métodos, son funciones que están metidas en un objeto
 gato.salir();
 gato.dormir();
+*/
+let texto = "Gato";
+console.log(texto.length);
+console.log(texto.toUpperCase());
+
+console.log(typeof texto);
+
+let texto2 = new String("Perro"); //no es recomendada, estamos usando un constructor
+console.log(texto2)
+console.log(texto2.toUpperCase());
+
+let arr = ["Azul", "Blanco"]
+console.log(arr)
