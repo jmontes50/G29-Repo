fbb7741dfa0ac8225ab3e6f5c1b36f306ef876b5
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-11 22:53:03 -0500

    avance

---
+++ Semana7/Día1/07-metodos-arrays.js
@@ -0,0 +1,30 @@
+//item, cada item del array de forma individual
+let frutas = ["Kiwi", "Naranja", "Piña", "Fresas"];
+
+console.log(frutas)
+
+//para algo similar como el for que lo haga repetidamente
+//forEach, va a recibir una función a ejecutar por cada item que encuentre en el array
+// arr.forEach(function(valor, i){})
+frutas.forEach(function(dato, indice, array) {
+  console.log(indice, dato, array);
+}) //99%
+
+//imperativo
+// for(let i = 0; i < frutas.length; i++){
+//   console.log(frutas[i])
+// }
+
+//Transformar
+let frutasMayusculas = frutas.map(function(dato, index) {
+  //es obligatorio retornar la expresión donde este transformado el item/dato
+  return `${index} - ${dato.toUpperCase()}`
+})
+
+console.log(frutasMayusculas);
+
+// let newFrutas = [];
+// for(let i = 0; i < frutas.length; i++){
+//   newFrutas.push(`${i} - ${frutas[i].toUpperCase()}`)
+// }
+// console.log(newFrutas)
