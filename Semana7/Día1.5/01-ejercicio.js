7626d428cdcf58082474644bfe7acf63658a9b3f
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-12 21:33:37 -0500

    avance

--- Semana7/Día1.5/01-ejercicio.js
+++ Semana7/Día1.5/01-ejercicio.js
@@ -18,7 +18,10 @@ gato
 //la aplicación debe permitir agregar, listar los productos (prompt y confirm)
 //CRUD  - Create Read Update Delete
 
-let almacen = [{ nombre: "Agua", costo: 1.0, stock: 10 }];
+let almacen = [
+  { nombre: "Agua", costo: 1.0, stock: 10 },
+  { nombre: "chocolate", costo: 2, stock: 10 },
+];
 
 let opcion = 0;
 //0 working
@@ -31,17 +34,31 @@ do {
   console.log(opcion);
 
   if (opcion === 1) {
+    //crear
     let nuevoProducto = {};
 
-    console.log(nuevoProducto);
     let nombreProducto = prompt("Ingrese el nombre del producto");
     let costoProducto = Number(prompt("Ingrese el costo del producto"));
     let stockProducto = Number(prompt("Ingrese el stock del producto"));
     //agregando una nueva propiedad, armando el objeto
     nuevoProducto.nombre = nombreProducto;
     nuevoProducto.costo = costoProducto;
-    nuevoProducto.stock = stockProducto
+    nuevoProducto.stock = stockProducto;
 
     console.log(nuevoProducto);
+
+    almacen.push(nuevoProducto);
+    console.table(almacen);
+  } else if (opcion === 2) {
+    console.log(`Tienes ${almacen.length} productos almacenados`);
+    //leer
+    //recorrer todo el almacén
+    for (let i = 0; i < almacen.length; i++) {
+      let nombre = almacen[i].nombre;
+      let costo = almacen[i].costo;
+      let stock = almacen[i].stock;
+
+      console.log(`Tienes ${nombre} con un valor de S/ ${costo * stock}`);
+    }
   }
 } while (opcion !== 3);
