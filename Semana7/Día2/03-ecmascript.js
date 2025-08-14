f0d87bbe127b197891d55f948e0fc9fb8b5476a4
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-13 22:47:13 -0500

    avance

--- Semana7/Día2/03-ecmascript.js
+++ Semana7/Día2/03-ecmascript.js
@@ -19,4 +19,16 @@ const IGV = 0.18; //No es fácil cambiarlo, por decir que no se puede
 
 console.log(IGV); //Podemos referenciarlo
 
+const haceSol = confirm("Esta soleado hoy?");
 
+//Para expresiones que ya den un booleano No es necesario compararlas con un true o false
+// console.log(haceSol === true)
+// console.log(haceSol)
+// if(haceSol){
+//   //etc etc etc
+// }
+
+//Operador ternario
+// expresión_A_evaluar ? Si es verdadero : si es falso
+const consejo = haceSol === true ? "Si es V: Usa bloqueador" : "Si es F: Ponte abrigo";
+console.log(consejo);
