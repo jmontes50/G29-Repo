dc26b4319201f54382fac2075cb89b7d055829cc
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-13 22:35:56 -0500

    avance

---
+++ Semana7/Día2/03-ecmascript.js
@@ -0,0 +1,22 @@
+//VAR no se utiliza, no es SEGURO
+
+// var a = 10;
+
+// var a = 200;
+
+// console.log(a);
+
+let a = 10;
+
+// let a = 200; //No puede redeclararlo
+a = 120000000
+
+console.log(a);
+
+const IGV = 0.18; //No es fácil cambiarlo, por decir que no se puede
+
+// IGV = 0.15; //No podemos reasignarlo
+
+console.log(IGV); //Podemos referenciarlo
+
+
