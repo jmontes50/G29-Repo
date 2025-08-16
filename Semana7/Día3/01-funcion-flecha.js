c5b028b4b33aa44847bb352a0436fa3240b3bd11
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-15 19:29:04 -0500

    avance

---
+++ Semana7/Día3/01-funcion-flecha.js
@@ -0,0 +1,13 @@
+// const saludar = function (nombre) {
+//   console.log(`Hola soy ${nombre}`);
+// }
+
+//función flecha
+//const nombreFunción = (params...) => {
+//  return "Hola"
+// }
+const saludar = (nombre, apellidos) => {
+  return`Hola soy ${nombre} ${apellidos}`
+}
+
+console.log(saludar("Jorge", "Montesinos"));
