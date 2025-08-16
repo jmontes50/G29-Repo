558207c1567fa07844b1685b12538e9ede161e2f
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-15 19:39:58 -0500

    avance

--- Semana7/Día3/01-funcion-flecha.js
+++ Semana7/Día3/01-funcion-flecha.js
@@ -7,7 +7,27 @@
 //  return "Hola"
 // }
 const saludar = (nombre, apellidos) => {
-  return`Hola soy ${nombre} ${apellidos}`
+  //sentencia 1
+  //sentencia 2
+  //sentencia 3 ...
+  return `Hola soy ${nombre} ${apellidos}`
 }
 
 console.log(saludar("Jorge", "Montesinos"));
+
+//SI es SOLO 01 PARÁMETRO, podemos quitar los paréntesis y dejar solo al parámetro
+const descripcion = nombreProd => {
+  return `Este producto ${nombreProd} esta disponible`;
+}
+
+console.log(descripcion("Luces Led"));
+
+// const anadirDecimales = (numero) => {
+//   //.toFixed es un método que añade decimales a un número y retorna un string con decimales
+//   return numero.toFixed(2);
+// }
+const anadirDecimales = (numero) => numero.toFixed(2);
+
+
+console.log(anadirDecimales(100));
+
