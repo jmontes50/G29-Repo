c73c92c907985a7bdc776b83d4e9349f5464d990
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-20 22:03:30 -0500

    avance

--- Semana8/Día2/DOM/02-dom.js
+++ Semana8/Día2/DOM/02-dom.js
@@ -3,9 +3,24 @@ console.log("DOCUMENT:", document);
 //Selectores de JS
 const Titulo_elemento = document.getElementById("titulo");
 
-//... lo que obtenemos es un Elemento HTML, objeto Element
+//... lo que obtenemos es 01 Elemento HTML, objeto Element
 console.log(Titulo_elemento);
 
 Titulo_elemento.style.color = "red";
 
 console.log("Vemos su HTML interno:", Titulo_elemento.innerHTML);
+
+//El querySelectorAll y querySelector, funciona como un selector de CSS por ejemplo para class="parrafo" es ...
+const elementos_p = document.querySelectorAll(".parrafo");
+//todos los selectores de CSS
+// const elementos_p = document.querySelectorAll("p");
+
+console.log("Elementos párrafo:",elementos_p);
+
+//Los Nodelist obtenenidos por querySelectorAll podemos tratarlos como Arreglos
+elementos_p.forEach(function(item){
+  console.log(item);
+  //.style me permite acceder a cualquier propiedad de css
+  item.style.color = "blue";
+  item.style.fontWeight = "bold";
+})
