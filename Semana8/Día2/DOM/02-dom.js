d4489a0d1da251197193eedf80b6bcd5f1a1c767
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-20 22:18:07 -0500

    avance

--- Semana8/Día2/DOM/02-dom.js
+++ Semana8/Día2/DOM/02-dom.js
@@ -14,6 +14,8 @@ console.log("Vemos su HTML interno:", Titulo_elemento.innerHTML);
 const elementos_p = document.querySelectorAll(".parrafo");
 //todos los selectores de CSS
 // const elementos_p = document.querySelectorAll("p");
+//También existe el document.getElementsByClassName(".parrafos")
+//Pero no te da un Array
 
 console.log("Elementos párrafo:",elementos_p);
 
@@ -22,5 +24,13 @@ elementos_p.forEach(function(item){
   console.log(item);
   //.style me permite acceder a cualquier propiedad de css
   item.style.color = "blue";
+  //font-weight: bold; ---> camelCase ---> fontWeight
   item.style.fontWeight = "bold";
 })
+
+//si es 01 solo elemento, también..
+const inputText = document.querySelector("#input-texto");
+
+console.log("input texto:", inputText);
+
+console.log(inputText.getAttribute("type"));
