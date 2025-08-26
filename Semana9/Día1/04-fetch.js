a25df45e7a588736dbc491f043687c88fc366343
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-25 22:32:12 -0500

    avance

--- Semana9/Día1/04-fetch.js
+++ Semana9/Día1/04-fetch.js
@@ -1,10 +1,21 @@
 
 
-fetch("https://jsonplaceholder.typicode.com/users")
-.then((respuesta) => {
-  console.log("Respuesta:", respuesta); //es la respuesta HTTP, pueden pensar en ella como una guía de remisión
-  return respuesta.json();
-})
-.then((users) => {
-  console.log("Usuarios", users);
-})
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((respuesta) => {
+//   console.log("Respuesta:", respuesta); //es la respuesta HTTP, pueden pensar en ella como una guía de remisión
+//   return respuesta.json();
+// })
+// .then((users) => {
+//   console.log("Usuarios", users);
+// })
+
+const getUsers = async () => {
+  try {
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users =  await respuesta.json()
+    console.log(users)
+  } catch (error) {
+    console.log(error);
+  }
+}
+getUsers();
