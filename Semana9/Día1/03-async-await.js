7f3a3a882fef7db9f90e1857d5fc14b0f67dcd27
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-25 21:51:04 -0500

    avance

--- Semana9/Día1/03-async-await.js
+++ Semana9/Día1/03-async-await.js
@@ -18,6 +18,31 @@ const operacion1 = async () => {
   }
 }
 
+//función clásica pero con async
+async function operacion2() {
+  const suerte = Math.random();
+  console.log(suerte);
+  if(suerte > 0.4){
+    return "La operación2 fue exitosa!"; //resolve
+  }else{
+    throw "La operación2 fallo"; //reject
+  }
+}
+
+const ejecutar = async () => {
+  try {
+    //intentando ejecutar
+    const res = await operacion1();
+    const res2 = await operacion2();
+    console.log(res);
+    console.log(res2)
+  } catch (error) {
+    //lo que hacemos para manejar el error
+    console.log("Errooor", error);
+  }
+}
+ejecutar();
+
 // operacion1()
 // .then((respuesta) => {
 //   console.log(respuesta);
@@ -25,15 +50,3 @@ const operacion1 = async () => {
 // .catch((error) => {
 //   console.log(error);
 // })
-
-const ejecutar = async () => {
-  try {
-    //intentando ejecutar
-    const res = await operacion1();
-    console.log(res);
-  } catch (error) {
-    //lo que hacemos para manejar el error
-    console.log("Errooor", error)
-  }
-}
-ejecutar();
