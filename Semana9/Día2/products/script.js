320e3dddb86afeccd5442937af4389f89da09153
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-27 19:55:58 -0500

    avance

--- Semana9/Día2/products/script.js
+++ Semana9/Día2/products/script.js
@@ -1,3 +1,13 @@
 //script.js
+import { obtenerProductos } from "./productService.js";
+
 const seccionProductos = document.getElementById("seccion-productos");
 
+
+
+
+const App = async () => {
+  const productosObtenidos = await obtenerProductos();
+  console.log(productosObtenidos)
+}
+App();
