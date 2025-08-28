488b659c458bd66c23605f7c48d4c24d9b7db523
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-27 20:20:51 -0500

    avance

--- Semana9/Día2/products/script.js
+++ Semana9/Día2/products/script.js
@@ -4,15 +4,18 @@ import ProductComponent from "./ProductComponent.js";
 
 const seccionProductos = document.getElementById("seccion-productos");
 
-
+const dibujarProductos = (productosObtenidos) => {
+  seccionProductos.innerHTML = ""; //limpiando
+  productosObtenidos.forEach((producto) => {
+      const divProducto = ProductComponent(producto);
+    seccionProductos.appendChild(divProducto);
+  });
+}
 
 
 const App = async () => {
   const productosObtenidos = await obtenerProductos();
   // console.log(productosObtenidos)
-  productosObtenidos.forEach((producto) => {
-    const divProducto = ProductComponent(producto);
-    seccionProductos.appendChild(divProducto);
-  });
+  dibujarProductos(productosObtenidos)
 }
 App();
