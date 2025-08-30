d1701575df3d5652dc00a4fe5095e9f1bedca023
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-29 20:22:39 -0500

    avance

---
+++ Semana9/Día3/01-react/src/App.jsx
@@ -0,0 +1,10 @@
+//1. El nombre de los componentes siempre tiene que comenzar con mayuscula
+const App = () => {
+  const teams = "Equipos";
+  //codigooo que desee
+  //1. tiene que retornar JSX (JS extensión) que es como html
+  return (<h1>Hola Estudiantes</h1>);
+}
+
+//2do se exporta por default
+export default App;
