44b347aa0778c07272d4e15452f202d625a62df9
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-29 22:34:51 -0500

    avance

--- Semana9/Día3/02-hooks/src/App.jsx
+++ Semana9/Día3/02-hooks/src/App.jsx
@@ -8,15 +8,19 @@ const App = () => {
   console.log(setNumero)
 
   const cambiarNumero = () => {
-    setNumero(50);
+    setNumero(numero + 1);
     // numero = 50;
     // console.log("Ejecutando cambiar Numero", numero);
   };
+  const reducirNumero = () => {
+    setNumero(numero - 1);
+  }
 
   return (
     <>
       <h1>El número es: {numero}</h1>
-      <button onClick={cambiarNumero}>Cambiar</button>
+      <button onClick={cambiarNumero}>Aumentar</button>
+      <button onClick={reducirNumero}>Reducir</button>
     </>
   );
 };
