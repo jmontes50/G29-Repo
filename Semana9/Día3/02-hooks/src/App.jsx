5499a3970a8200b52df521e97b5aca5c67df8249
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-29 22:51:31 -0500

    avance

--- Semana9/Día3/02-hooks/src/App.jsx
+++ Semana9/Día3/02-hooks/src/App.jsx
@@ -4,8 +4,9 @@ const App = () => {
   // let numero = 10;
   // const [estado, funcionActualizaElEstado] = useState(valorInicialEstado)
   const [numero, setNumero] = useState(10);
+  const [texto, setTexto] = useState("Hola!!!");
   //c/vez que cambie un estado, La interfaz se actualizara pero con el estado actualizado
-  console.log(setNumero)
+  // console.log(setNumero)
 
   const cambiarNumero = () => {
     setNumero(numero + 1);
@@ -14,6 +15,11 @@ const App = () => {
   };
   const reducirNumero = () => {
     setNumero(numero - 1);
+  };
+
+  const manejarInput = (event) => {
+    console.log(event.target.value);
+    setTexto(event.target.value);
   }
 
   return (
@@ -21,6 +27,11 @@ const App = () => {
       <h1>El número es: {numero}</h1>
       <button onClick={cambiarNumero}>Aumentar</button>
       <button onClick={reducirNumero}>Reducir</button>
+      <hr/>
+      <h3>El texto es: {texto}</h3>
+      {/* En el caso de los inputs hay una idea, que se llama componentes controlados -> TODO INPUT DEBE ESTAR DE ALGUNA MANERA AMARRADO A UN ESTADO */}
+      {/* onChange se dispara cuando un input cambia de valor */}
+      <input type="text" value={texto} onChange={manejarInput} />
     </>
   );
 };
