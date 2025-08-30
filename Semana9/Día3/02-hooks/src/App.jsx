e76a76b8760983e09e815b446bb9ee39031a5b8f
Jorge Montesinos <osmarmontes50@gmail.com>
2025-08-29 22:31:46 -0500

    avance

--- Semana9/Día3/02-hooks/src/App.jsx
+++ Semana9/Día3/02-hooks/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 const App = () => {
-  let numero = 10;
+  // let numero = 10;
+  // const [estado, funcionActualizaElEstado] = useState(valorInicialEstado)
+  const [numero, setNumero] = useState(10);
+  //c/vez que cambie un estado, La interfaz se actualizara pero con el estado actualizado
+  console.log(setNumero)
 
   const cambiarNumero = () => {
-    console.log("Ejecutando cambiar Numero");
-    numero = 50;
+    setNumero(50);
+    // numero = 50;
+    // console.log("Ejecutando cambiar Numero", numero);
   };
 
   return (
