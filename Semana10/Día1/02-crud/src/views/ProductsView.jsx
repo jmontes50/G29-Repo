89a496c7b5616760d9e478a4f18d55e097837e7c
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-03 20:26:01 -0500

    avance

--- Semana10/Día1/02-crud/src/views/ProductsView.jsx
+++ Semana10/Día1/02-crud/src/views/ProductsView.jsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import { readProducts } from "../services/productService";
 
 const ProductsView = () => {
-  const [products, setProducts] = useState([]);
+  //truthy y falsy
+  const [products, setProducts] = useState(null);
   /**1. Objetivo:mostrar productos
    * 2. Donde estan los productos -> mockapi
    * 3. Cómo los obtengo???? -> axios/service
-   * 4. Cómo los muestro??? ->
+   * 4. Cómo los muestro??? -> transformandolo a algo que el navegador entienda
    */
 
   useEffect(() => {
@@ -16,21 +17,28 @@ const ProductsView = () => {
         console.log(productsObtained);
         setProducts(productsObtained);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     };
     getProducts();
   }, []);
 
   //Renderizado de listas
-
-  return <div>
-    {products.map((prod) => (
-      <div>
-        <h3>{prod.producto_nombre}</h3>
-      </div>
-    ))}
-  </div>;
+  return (
+    <div>
+      {/* {products.length > 0 && products ? ( // verificamos que la
+      // longitud sea mayor que 0 */}
+      {products ? ( // verificando el tipo de dato
+        products.map((prod) => ( //transformamos los productos a algo entendible por el navegador
+          <div key={prod.id}>
+            <h3>{prod.producto_nombre}</h3>
+          </div>
+        ))
+      ) : ( //si no hay productos muestro otro mensaje
+        <p>No hay productos aún</p>
+      )}
+    </div>
+  );
 };
 
 export default ProductsView;
