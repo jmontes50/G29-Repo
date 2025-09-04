59fa2910b25729a59f05282c6457a1b1108fe37a
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-03 21:04:50 -0500

    avance

--- Semana10/Día1/02-crud/src/components/TableData.jsx
+++ Semana10/Día1/02-crud/src/components/TableData.jsx
@@ -2,7 +2,22 @@
 const TableData = ({ data }) => { //recordemos que los props son un objeto
   console.log("TableData", data)
   return (
-    <div>TableData</div>
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Descripción</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr>
+            <td>{item.producto_nombre}</td>
+            <td>{item.producto_descripcion}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
