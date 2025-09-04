b80feb28618cfedbb67dca5fdf6b3681582f53eb
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-03 19:46:23 -0500

    avance

--- Semana10/Día1/02-crud/src/views/ProductsView.jsx
+++ Semana10/Día1/02-crud/src/views/ProductsView.jsx
@@ -3,13 +3,20 @@ import { readProducts } from "../services/productService";
 
 const ProductsView = () => {
   const [products, setProducts] = useState(null);
+  /**1. Objetivo:mostrar productos
+   * 2. Donde estan los productos -> mockapi
+   * 3. Cómo los obtengo???? -> axios/service
+   * 4. Cómo los muestro??? ->
+   */
 
-  useEffect((readProducts) => {
+  useEffect(() => {
     const getProducts = async () => {
       try {
         const respuesta = await readProducts();
         console.log(respuesta);
-      } catch (error) {}
+      } catch (error) {
+        console.log(error)
+      }
     };
     getProducts();
   }, []);
