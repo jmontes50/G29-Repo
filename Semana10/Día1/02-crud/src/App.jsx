752692b1fa28988cc324f3a51ae403469f34b870
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-03 22:30:46 -0500

    avance

--- Semana10/Día1/02-crud/src/App.jsx
+++ Semana10/Día1/02-crud/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 //rafce snippet react
 import ProductsView from "./views/ProductsView";
 
 const App = () => {
   return (
-    <div>App
-      <ProductsView />
-    </div>
+    // <div>App
+    //   <ProductsView />
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductsView />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
