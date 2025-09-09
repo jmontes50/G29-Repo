02803e16e7b7e194f812e4f674a0bb7ff97becd1
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-08 22:37:53 -0500

    avance

--- Semana10/Día1/02-crud/src/components/Navbar.jsx
+++ Semana10/Día1/02-crud/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
+//Link es el equivalente a hipervinculos pero en react manteniendo la SPA
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Admin</a>
+        {/* <a className="btn btn-ghost text-xl">Admin</a> */}
+        <Link className="btn btn-ghost text-xl" to="/">
+          Admin
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            {/* <a href="/crear">Link</a> */}
+            <Link to="/crear">Crear Producto</Link>
           </li>
         </ul>
       </div>
