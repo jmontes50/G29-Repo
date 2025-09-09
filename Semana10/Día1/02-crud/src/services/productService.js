334fd22ace499d2068f293024ada6003dbf8a9a3
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-08 21:35:48 -0500

    avance

--- Semana10/Día1/02-crud/src/services/productService.js
+++ Semana10/Día1/02-crud/src/services/productService.js
@@ -22,8 +22,13 @@ const createProduct = async (newProduct) => {
   try {
     //axios tiene métodos .get .post .put .delete para cada verbo http a usar
     const result = await axios.post("https://68afa145b91dfcdd62bcb6b1.mockapi.io/productos", newProduct );
-    console.log(result);
-    return result.data;
+    // console.log(result);
+    if(result.status === 201) {
+      return result.data;
+    } else {
+      throw new Error("error al crear recurso");
+    }
+
   } catch (error) {
     console.log(error);
     throw error;
