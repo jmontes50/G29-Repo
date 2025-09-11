cafa78fd19e16c73f27908f4a50cf45e82d37eeb
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-10 20:01:30 -0500

    avance

--- Semana10/Día1/02-crud/src/views/EditProductView.jsx
+++ Semana10/Día1/02-crud/src/views/EditProductView.jsx
@@ -1,6 +1,10 @@
-
+import { useParams } from "react-router-dom";
 
 const EditProductView = () => {
+
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>EditProductView</div>
   )
