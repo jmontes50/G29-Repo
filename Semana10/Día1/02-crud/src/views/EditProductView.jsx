3b080859686c7767efa35369ac8d7ef09763d207
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-10 20:23:25 -0500

    avance

--- Semana10/Día1/02-crud/src/views/EditProductView.jsx
+++ Semana10/Día1/02-crud/src/views/EditProductView.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { readProductById } from "../services/productService";
 
 const EditProductView = () => {
+  const [product, setProduct] = useState({
+    producto_nombre:"",
+    producto_precio:0,
+    producto_descripcion:"",
+    producto_imagen:"",
+  })
 
-  const { id } = useParams();
+  const { id } = useParams(); //recordemos que esto viene de la url
+  //ej. /editar/5
   console.log(id);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const data = await readProductById(id)
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProduct();
+  }, [])
 
   return (
     <div>EditProductView</div>
