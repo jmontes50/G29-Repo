bc9c0f4628a878b98d9ea42c4e9f2e5747208e29
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-12 21:41:26 -0500

    avance

--- Semana10/Día1/02-crud/src/services/supabaseService.js
+++ Semana10/Día1/02-crud/src/services/supabaseService.js
@@ -1,12 +1,12 @@
 import { supabase } from "../config/supabaseConfig";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const BUCKET_NAME = "products_bucket";
 
 const uploadFile = async (newFile) => {
   //newFile tiene una propiedad name ej. Foto.png
   try {
-    const originalName = newFile.name.split('.'); //separamos el nombre de la extensión
+    const originalName = newFile.name.split("."); //separamos el nombre de la extensión
     const extension = originalName[originalName.length - 1];
 
     const newName = uuidv4(); //nos deberia dar un nombre alfanúmerico '423hsdfsd-3244fg43-fdgsd23'
@@ -16,15 +16,18 @@ const uploadFile = async (newFile) => {
     const { data, error } = await supabase.storage
       .from(BUCKET_NAME) //en from va el nombre del bucket
       .upload(newNameWithExtension, newFile);
-    console.log(data);
     if (error) {
       throw new Error(error);
     }
+    // console.log(data);
+    //data tiene una propiedad path que es necesaria para b uscar y retornar la url pública
+    const filePublicUrl = supabase.storage
+      .from(BUCKET_NAME)
+      .getPublicUrl(data.path);
+    return filePublicUrl;
   } catch (error) {
     console.log(error);
   }
 };
 
-export {
-  uploadFile
-}
+export { uploadFile };
