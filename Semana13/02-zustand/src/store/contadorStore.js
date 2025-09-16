e493b6c8495604488584a1f8c7a4832890a9c9ff
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-15 22:54:31 -0500

    avance

--- Semana13/02-zustand/src/store/contadorStore.js
+++ Semana13/02-zustand/src/store/contadorStore.js
@@ -1,22 +1,27 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 //set es una función que nos permite "actualizar" el estado y modificar lo que indiquemos en el set
-const useContadorStore = create((set) => ({
-  numero: 0,
-  texto: "Oso",
-  //actions
-  incrementar: () => {
-    set((state) => ({ numero: state.numero + 1 }));
-  },
-  reducir: () => {
-    set((state) => ({ numero: state.numero - 1 }));
-  },
-  cambiarTexto: () => {
-    set({ texto: "Pingüino"})
-  },
-  concatenarTexto: () => {
-    set((state) => ({ texto: state.texto + " Tortuga"}))
-  }
-}));
+const useContadorStore = create(
+  devtools( //lo pueden quitar o comentar despues
+    (set) => ({
+      numero: 0,
+      texto: "Oso",
+      //actions
+      incrementar: () => {
+        set((state) => ({ numero: state.numero + 1 }));
+      },
+      reducir: () => {
+        set((state) => ({ numero: state.numero - 1 }));
+      },
+      cambiarTexto: () => {
+        set({ texto: "Pingüino"})
+      },
+      concatenarTexto: () => {
+        set((state) => ({ texto: state.texto + " Tortuga"}))
+      }
+    })
+  )
+);
 
 export default useContadorStore;
